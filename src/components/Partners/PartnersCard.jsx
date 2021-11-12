5277abc559ba4b9d1f61771aec6b29a25416133d
Fpsska <fpsska1337@gmail.com>
2021-11-12 15:02:30 +0300

    feat: add dynamic images render for PartnersCard component

--- src/components/Partners/PartnersCard.jsx
+++ src/components/Partners/PartnersCard.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 
-
 const PartnerCard = (props) => {
     return (
         <div className="partner-card">
-            <img className="partner-card__image" src={`../../assets/images/${props.image}`} alt="company" />
+            <img className="partner-card__image" src={require(`../../assets/images/${props.image}`).default} alt="company" />
         </div>
     )
 }
