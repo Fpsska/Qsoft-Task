2a27f1999f1d4fde6a9f0ae05807d3c6c85e5b02
Fpsska <fpsska1337@gmail.com>
2021-11-12 18:18:02 +0300

    refactor: HTML-structure of PartnersCard component

--- src/components/Partners/PartnersCard.jsx
+++ src/components/Partners/PartnersCard.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 
 const PartnerCard = (props) => {
-    return (
-        <div className="partner-card">
-            <img className="partner-card__image" src={require(`../../assets/images/${props.image}`).default} alt="company" />
-        </div>
-    )
-}
+  return (
+    <figure className="partner-card">
+      <img
+        className="partner-card__image"
+        src={require(`../../assets/images/${props.image}`).default}
+        alt="company"
+      />
+    </figure>
+  );
+};
 
 export default PartnerCard;
